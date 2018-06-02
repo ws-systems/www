6d96e7893e8a21ca7eb1476b21451fc29dfb6c46
Tom Paulus <tom@tompaulus.com>
2018-06-02 12:59:37 -0700

    Updated Gulpfile and Frontend Build Method

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -10,14 +10,56 @@ var gulp = require('gulp'),
 var config = {
     stylesPath: 'styles/sass',
     jsPath: 'styles/js',
+    localLibsPath: 'styles/libs',
     imagesPath: 'images',
     outputDir: 'assets'
 };
 
-gulp.task('font-awesome', function () {
-    return gulp.src('./node_modules/font-awesome/fonts/**.*')
-        .pipe(gulp.dest(config.outputDir + '/fonts'));
+gulp.task('font-awesome-css', function (cb) {
+    pump([
+        gulp.src(config.localLibsPath + '/fa/*'),
+        filter('**/*.css'),
+        concat("fontawesome.css"),
+        gulp.dest(config.outputDir + '/css')
+    ], cb);
 });
+
+gulp.task('font-awesome-js', function (cb) {
+    pump([
+        gulp.src(config.localLibsPath + '/fa/*'),
+        filter('**/*.js'),
+        uglify(),
+        concat('fontawesome.js'),
+        gulp.dest(config.outputDir + '/js')
+    ], cb);
+});
+
+
+gulp.task('jquery', function (cb) {
+    pump([
+        gulp.src('./node_modules/jquery/dist/*'),
+        filter('**/jquery.min.*'),
+        gulp.dest(config.outputDir + '/js')
+    ], cb);
+});
+
+gulp.task('sweet-alerts', function () {
+    return gulp.src('./node_modules/sweetalert/dist/sweetalert.min.js')
+        .pipe(gulp.dest(config.outputDir + '/js'));
+});
+
+
+gulp.task('lightbox', function () {
+    return gulp.src('./node_modules/lightbox2/dist/js/lightbox.min.*')
+            .pipe(gulp.dest(config.outputDir + '/js')) &&
+        gulp.src('./node_modules/lightbox2/dist/images/**/*')
+            .pipe(gulp.dest(config.outputDir + '/images')) &&
+        gulp.src('./node_modules/lightbox2/dist/css/lightbox.min.css')
+            .pipe(gulp.dest(config.outputDir + '/css'));
+});
+
+gulp.task('libs', gulp.parallel('jquery', 'font-awesome-css', 'font-awesome-js', 'sweet-alerts', 'lightbox'));
+
 
 gulp.task('images', function () {
     return gulp.src(config.imagesPath + '/*')
@@ -38,26 +80,6 @@ gulp.task('css', function (cb) {
             autoprefixer(),
             gulp.dest(config.outputDir + '/css')
         ], cb);
-});
-
-
-gulp.task('jquery', function () {
-    return gulp.src('./node_modules/jquery/dist/jquery.min.js')
-        .pipe(gulp.dest(config.outputDir + '/js'));
-});
-
-gulp.task('sweet-alerts', function () {
-    return gulp.src('./node_modules/sweetalert/dist/sweetalert.min.js')
-        .pipe(gulp.dest(config.outputDir + '/js'));
-});
-
-gulp.task('lightbox', function () {
-    return gulp.src('./node_modules/lightbox2/dist/js/lightbox.min.*')
-        .pipe(gulp.dest(config.outputDir + '/js')) &&
-        gulp.src('./node_modules/lightbox2/dist/images/**/*')
-            .pipe(gulp.dest(config.outputDir + '/images')) &&
-        gulp.src('./node_modules/lightbox2/dist/css/lightbox.min.css')
-            .pipe(gulp.dest(config.outputDir + '/css'));
 });
 
 gulp.task('js', function (cb) {
@@ -88,6 +110,6 @@ gulp.task('watch', function () {
     gulp.watch([config.imagesPath + '/**/*'], ['images']);
 });
 
-gulp.task('build', gulp.series('images', 'css', 'jquery', 'sweet-alerts', 'lightbox', 'js-ie', 'js', 'font-awesome'));
+gulp.task('build', gulp.series('libs', 'images', 'css', 'js-ie', 'js'));
 
 gulp.task('default', gulp.parallel('build'));
